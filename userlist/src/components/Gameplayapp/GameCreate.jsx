0168bb36d53b7b93e54eb6d72aa76d49b41e9081
Martin7n <122726440+Martin7n@users.noreply.github.com>
2025-10-04 14:02:19 +0300

    ctrl forms in gameCreate

--- userlist/src/components/Gameplayapp/GameCreate.jsx
+++ userlist/src/components/Gameplayapp/GameCreate.jsx
@@ -1,14 +1,46 @@
+import { useState } from "react";
+
+
+const initialValues = {
+            title: '',
+            category: '',
+            maxLevel: '',
+            imageUrl: '',
+            summary: '',
+}
+
 
 const GameCreate = () => {
+    
+    const [formData, setFormData] = useState(
+        initialValues
+        );
+
+    
+    const onSubmit = (e) => {
+        e.preventDefault();
+        console.log('Submitting')
+        //!! todo - validation, service, navigate, refactoring into hook
+        console.log(formData)
+
+    };
+    const OnChange = (e) => {
+        const {name, value} = e.target;
+
+        setFormData(data => ({...data, [name]: value}))
+        console.log(formData)
+
+    };
+    
 
 
 
 
     return (
 
-        <section id="create-page" class="auth">
-            <htmlForm id="create">
-                <div class="container">
+        <section id="create-page" className="auth">
+            <form id="create" onSubmit={onSubmit}>
+                <div className="container">
 
                     <h1>Create Game</h1>
                     <label htmlFor="leg-title">Legendary title:</label>
@@ -16,14 +48,20 @@ const GameCreate = () => {
                     type="text"
                     id="title"
                     name="title"
-                    placeholder="Enter game title..."/>
+                    placeholder="Enter game title..."
+                    value={formData.title}
+                    onChange={OnChange}
+                    />
 
                     <label htmlFor="category">Category:</label>
                     <input 
                     type="text"
                     id="category"
                     name="category"
-                    placeholder="Enter game category..."/>
+                    placeholder="Enter game category..."
+                    value={formData.category}
+                    onChange={OnChange}
+                    />
 
                     <label htmlFor="levels">MaxLevel:</label>
                     <input 
@@ -31,23 +69,33 @@ const GameCreate = () => {
                     id="maxLevel"
                     name="maxLevel"
                     min="1" 
-                    placeholder="1"/>
+                    placeholder="1"
+                    value={formData.maxLevel}
+                    onChange={OnChange}
+                    />
 
                     <label htmlFor="game-img">Image:</label>
                     <input 
                     type="text"
                     id="imageUrl"
                     name="imageUrl"
-                    placeholder="Upload a photo..."/>
+                    placeholder="Upload a photo..."
+                    value={formData.imageUrl}
+                    onChange={OnChange}
+                    />
 
                     <label htmlFor="summary">Summary:</label>
-                    <textarea name="summary" id="summary"></textarea>
-                    <input class="btn submit" 
+                    <textarea name="summary" 
+                    id="summary"
+                    value={formData.summary}
+                    onChange={OnChange}
+                    > </textarea>
+                    <input className="btn submit" 
                     type="submit"
-                    value="Create Gam
+                    value="Create Game
                     "/>
                 </div>
-                            </htmlForm>
+                            </form>
         </section>
         
     )
